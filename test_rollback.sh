500fded96fa73dd1ba8963db228b59886e90461d
asvarov <45462669+asvarov@users.noreply.github.com>
2019-10-20 15:21:19 +0300

    Create test_rollback.sh

---
+++ test_rollback.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ $(docker ps -f name=green -q) ]
+then
+    ENV="green"
+    OLD="blue"
+else
+    ENV="blue"
+    OLD="green"
+fi
+
+echo "Checkiong "$OLD" container"
+if  curl --output /dev/null --silent --retry 5 --show-error --head --fail ${1}
+  then
+        echo ""
+        echo "Removing "$OLD" container"
+        docker rm -f $(docker ps -f name=$OLD -q)
+  else
+        echo "Removing "$ENV" container"
+        docker rm -f $(docker ps -f name=$ENV -q)
+
+        echo "Waiting..."
+        sleep 5s
+
+        echo "Starting "$OLD" container"
+        docker-compose --project-name=$OLD up -d
+        exit 1
+  fi
