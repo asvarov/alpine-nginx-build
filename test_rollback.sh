f13b2f92844a4bae0a25c85252075d9d62ed83af
asvarov <45462669+asvarov@users.noreply.github.com>
2019-10-20 17:21:45 +0300

    Update test_rollback.sh

--- test_rollback.sh
+++ test_rollback.sh
@@ -4,14 +4,23 @@ export DEPLOY_ENV_NEW=${1}
 export DEPLOY_ENV_OLD=${2}
 export EXTERNAL_IP=${3}
 
+if [ "x$DEPLOY_ENV_NEW" != "x" ]; then
+  echo "DEPLOY_ENV_NEW is null > exit 1" && exit 1
+elif [ "x$DEPLOY_ENV_OLD" != "x" ]; then
+  echo "DEPLOY_ENV_OLD is null > exit 1" && exit 1
+elif [ "x$EXTERNAL_IP" != "x" ]; then
+  echo "EXTERNAL_IP is null > exit 1" && exit 1  
+fi;
+
+
 echo "Checkiong "$DEPLOY_ENV_NEW" container"
 if  curl --output /dev/null --silent --retry 5 --show-error --head --fail ${EXTERNAL_IP}
   then
         echo "Removing "$DEPLOY_ENV_OLD" container"
-        #docker rm -f $(docker ps -af name=$DEPLOY_ENV_OLD -q)
+        docker rm -f $(docker ps -af name=${DEPLOY_ENV_OLD} -q)
   else
         echo "Removing "$DEPLOY_ENV_NEW" container"
-        #docker rm -f $(docker ps -af name=$DEPLOY_ENV_NEW -q)
+        docker rm -f $(docker ps -af name=${DEPLOY_ENV_NEW} -q)
 
         echo "Waiting..."
         sleep 5s
