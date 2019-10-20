d75baad975e87d7216b343465b1d90c584cdc039
asvarov <45462669+asvarov@users.noreply.github.com>
2019-10-20 16:48:12 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
-if [ $(docker ps -f name=blue -q) ]
-then
-    ENV="green"
-    OLD="blue"
-else
-    ENV="blue"
-    OLD="green"
-fi
 
-echo $ENV > env_new.txt
-echo $OLD > env_old.txt
-
-echo "Stopping "$OLD" container"
-docker-compose --project-name=$OLD stop
+echo "Stopping "$DEPLOY_ENV_OLD" container"
+docker-compose --project-name=$DEPLOY_ENV_OLD stop
 
 echo "Waiting..."
 sleep 5s
 
-echo "Starting "$ENV" container"
-docker-compose --project-name=$ENV up -d
+echo "Starting "$DEPLOY_ENV_NEW" container"
+docker-compose --project-name=$DEPLOY_ENV_NEW up -d
